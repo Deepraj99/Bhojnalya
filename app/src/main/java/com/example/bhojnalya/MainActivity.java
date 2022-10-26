9f0231a76aec46dd3cb720490d6047330eef12b6
Deepak Prajapati <dp0774138@gmail.com>
2022-10-26 15:33:14 +0530

    navigation bar implemented

--- app/src/main/java/com/example/bhojnalya/MainActivity.java
+++ app/src/main/java/com/example/bhojnalya/MainActivity.java
@@ -1,16 +1,53 @@
 package com.example.bhojnalya;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.bhojnalya.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.list:
+                    replaceFragment(new ListFragment());
+                    break;
+                case R.id.scanner:
+                    replaceFragment(new ScannerFragment());
+                    break;
+                case R.id.comments:
+                    replaceFragment(new CommentFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
